5043be09c13e037ad32b988c10d9936c317302ac
AKurganoff <ale2003kurganoff@gmail.com>
2025-11-07 12:16:06 -0300

    Add files via upload

---
+++ Excepcion/AutenticacionException.java
@@ -0,0 +1,8 @@
+package isi.deso.Excepcion; 
+
+/** Excepción personalizada de autenticacion. */
+public class AutenticacionException extends MainException { 
+    public AutenticacionException(String m){ 
+        super(m);
+    } 
+}
