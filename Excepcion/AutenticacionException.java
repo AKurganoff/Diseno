86c9239f1284f7e1c84fa794b768856c84132d66
nacho-mazzoni <123500539+nacho-mazzoni@users.noreply.github.com>
2025-11-08 00:31:09 -0300

    commit nacho con .gitignore y docker-compose

--- Excepcion/AutenticacionException.java
+++
@@ -1,8 +0,0 @@
-package isi.deso.Excepcion; 
-
-/** Excepción personalizada de autenticacion. */
-public class AutenticacionException extends MainException { 
-    public AutenticacionException(String m){ 
-        super(m);
-    } 
-}
